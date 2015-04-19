d3ea13a34b1ed1e0f8a8ebc6847cc529a48d73ef
kknguyen <kknguye1@uw.edu>
2015-04-19 16:38:22 -0700

    Menu added
    Play button loads test level

---
+++ AFTD/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	private bool isFirstMenu = true;
+	private bool isGameMenu = false;
+	private bool isOptionsMenu = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI () {
+		FirstMenu ();
+		LoadGameMenu ();
+		OptionsMenu ();
+	}
+
+	void FirstMenu () 
+	{
+		if (isFirstMenu) 
+		{
+			// Change text to content when assets are created
+			if (GUI.Button (new Rect(Screen.width/2-100,Screen.height/2-100, 200, 50), "Play")) 
+			{
+				Application.LoadLevel ("LevelOneTest");
+			}
+
+			// Change text to content when assets are created
+			if (GUI.Button (new Rect(Screen.width/2-100,Screen.height/2-25, 200, 50), "Options")) 
+			{
+
+			}
+
+			// Change text to content when assets are created
+			if (GUI.Button (new Rect(Screen.width/2-100,Screen.height/2+50, 200, 50), "Quit")) 
+			{
+				Application.Quit ();
+			}
+
+		}
+	}
+
+	void LoadGameMenu()
+	{
+
+	}
+
+	void OptionsMenu()
+	{
+
+	}
+}
